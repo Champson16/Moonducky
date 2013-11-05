cca78814e155a59faa8019fc59bb118b23625f2a
Jonathan Beebe <jon@lanica.co>
2013-11-05 12:22:13 -0800

    Increment build number to 0.9.0 and fix issue where all objects are black on device.

--- main.lua
+++ main.lua
@@ -10,12 +10,12 @@ require("modules.pinchlib")
 local storyboard = require('modules.stage');
 
 _G.APP_ID = 'com.fatredcouch.moonducky.songstudio.artcenter';
-_G.APP_VERSION = '0.1.0';
+_G.APP_VERSION = '0.9.0';
 
 display.setStatusBar(display.HiddenStatusBar);
 display.setDefault("background", 0.5, 0.5, 0.5);
-display.setDefault("textureWrapX", "repeat");
-display.setDefault("textureWrapY", "repeat");
+--display.setDefault("textureWrapX", "repeat");
+--display.setDefault("textureWrapY", "repeat");
 math.randomseed( os.time() )  -- make math.random() more random
 
 storyboard.gotoScene('scenes.ArtCenter.Scene');
