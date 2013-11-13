8559bf88f2ad1663b0197616e92620ffc16ace17
Jonathan Beebe <jon@lanica.co>
2013-11-12 16:46:30 -0800

    Increment version to 0.9.4

--- main.lua
+++ main.lua
@@ -10,7 +10,7 @@ require("FRC_Modules.FRC_MultiTouch.FRC_PinchLib");
 local FRC_SceneManager = require('FRC_Modules.FRC_SceneManager.FRC_SceneManager');
 
 _G.APP_ID = 'com.fatredcouch.moonducky.songstudio.artcenter';
-_G.APP_VERSION = '0.9.3';
+_G.APP_VERSION = '0.9.4';
 
 display.setStatusBar(display.HiddenStatusBar);
 display.setDefault("background", 0.5, 0.5, 0.5);
