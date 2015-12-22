4a71b40cb9ff4aa3496cededfa5f211557fb70f7
Terry R. Schussler <tschussler@fatredcouch.com>
2015-12-21 17:45:25 -0800

    Updated Home animation and artwork
    Signed-off-by: Terry R. Schussler <tschussler@fatredcouch.com>

---
+++ FRC_Assets/MDMT_Assets/Animation/XMLData/MDMT_LandingPage_MarqueeSign.lua
@@ -0,0 +1,27 @@
+xmltable = {
+	Animation = {
+		Part = {
+			name = "MDMT_LandingPage_MarqueeSign",
+			Frame = {
+				[1] = {
+					y = "-244.50",
+					x = "-1.50",
+					index = "0"
+				},
+				[2] = {
+					y = "-244.50",
+					x = "-1.50",
+					index = "1"
+				},
+				[3] = {
+					y = "-244.50",
+					x = "-1.50",
+					index = "2"
+				}
+			}
+		},
+		frameCount = "3",
+		name = "MDMT_LandingPage_MarqueeSign"
+	}
+}
+return xmltable;
