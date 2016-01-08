08cf812a6854e64a24485f0fbef6485bbd9fdde8
Terry R. Schussler <tschussler@fatredcouch.com>
2016-01-07 16:52:22 -0800

    Pre-processed Lua files for Jukebox animation
    Signed-off-by: Terry R. Schussler <tschussler@fatredcouch.com>

---
+++ FRC_Assets/MDMT_Assets/Animation/XMLData/MDMT_Jukebox_AnimDiscPlay_d.lua
@@ -0,0 +1,727 @@
+xmltable = {
+	Animation = {
+		Part = {
+			name = "MDMT_Jukebox_Background",
+			Frame = {
+				[1] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "0"
+				},
+				[2] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "1"
+				},
+				[3] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "2"
+				},
+				[4] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "3"
+				},
+				[5] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "4"
+				},
+				[6] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "5"
+				},
+				[7] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "6"
+				},
+				[8] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "7"
+				},
+				[9] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "8"
+				},
+				[10] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "9"
+				},
+				[11] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "10"
+				},
+				[12] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "11"
+				},
+				[13] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "12"
+				},
+				[14] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "13"
+				},
+				[15] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "14"
+				},
+				[16] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "15"
+				},
+				[17] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "16"
+				},
+				[18] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "17"
+				},
+				[19] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "18"
+				},
+				[20] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "19"
+				},
+				[21] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "20"
+				},
+				[22] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "21"
+				},
+				[23] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "22"
+				},
+				[24] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "23"
+				},
+				[25] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "24"
+				},
+				[26] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "25"
+				},
+				[27] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "26"
+				},
+				[28] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "27"
+				},
+				[29] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "28"
+				},
+				[30] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "29"
+				},
+				[31] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "30"
+				},
+				[32] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "31"
+				},
+				[33] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "32"
+				},
+				[34] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "33"
+				},
+				[35] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "34"
+				},
+				[36] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "35"
+				},
+				[37] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "36"
+				},
+				[38] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "37"
+				},
+				[39] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "38"
+				},
+				[40] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "39"
+				},
+				[41] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "40"
+				},
+				[42] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "41"
+				},
+				[43] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "42"
+				},
+				[44] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "43"
+				},
+				[45] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "44"
+				},
+				[46] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "45"
+				},
+				[47] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "46"
+				},
+				[48] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "47"
+				},
+				[49] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "48"
+				},
+				[50] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "49"
+				},
+				[51] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "50"
+				},
+				[52] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "51"
+				},
+				[53] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "52"
+				},
+				[54] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "53"
+				},
+				[55] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "54"
+				},
+				[56] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "55"
+				},
+				[57] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "56"
+				},
+				[58] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "57"
+				},
+				[59] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "58"
+				},
+				[60] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "59"
+				},
+				[61] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "60"
+				},
+				[62] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "61"
+				},
+				[63] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "62"
+				},
+				[64] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "63"
+				},
+				[65] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "64"
+				},
+				[66] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "65"
+				},
+				[67] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "66"
+				},
+				[68] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "67"
+				},
+				[69] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "68"
+				},
+				[70] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "69"
+				},
+				[71] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "70"
+				},
+				[72] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "71"
+				},
+				[73] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "72"
+				},
+				[74] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "73"
+				},
+				[75] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "74"
+				},
+				[76] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "75"
+				},
+				[77] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "76"
+				},
+				[78] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "77"
+				},
+				[79] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "78"
+				},
+				[80] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "79"
+				},
+				[81] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "80"
+				},
+				[82] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "81"
+				},
+				[83] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "82"
+				},
+				[84] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "83"
+				},
+				[85] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "84"
+				},
+				[86] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "85"
+				},
+				[87] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "86"
+				},
+				[88] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "87"
+				},
+				[89] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "88"
+				},
+				[90] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "89"
+				},
+				[91] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "90"
+				},
+				[92] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "91"
+				},
+				[93] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "92"
+				},
+				[94] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "93"
+				},
+				[95] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "94"
+				},
+				[96] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "95"
+				},
+				[97] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "96"
+				},
+				[98] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "97"
+				},
+				[99] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "98"
+				},
+				[100] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "99"
+				},
+				[101] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "100"
+				},
+				[102] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "101"
+				},
+				[103] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "102"
+				},
+				[104] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "103"
+				},
+				[105] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "104"
+				},
+				[106] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "105"
+				},
+				[107] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "106"
+				},
+				[108] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "107"
+				},
+				[109] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "108"
+				},
+				[110] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "109"
+				},
+				[111] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "110"
+				},
+				[112] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "111"
+				},
+				[113] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "112"
+				},
+				[114] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "113"
+				},
+				[115] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "114"
+				},
+				[116] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "115"
+				},
+				[117] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "116"
+				},
+				[118] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "117"
+				},
+				[119] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "118"
+				},
+				[120] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "119"
+				},
+				[121] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "120"
+				},
+				[122] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "121"
+				},
+				[123] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "122"
+				},
+				[124] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "123"
+				},
+				[125] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "124"
+				},
+				[126] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "125"
+				},
+				[127] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "126"
+				},
+				[128] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "127"
+				},
+				[129] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "128"
+				},
+				[130] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "129"
+				},
+				[131] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "130"
+				},
+				[132] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "131"
+				},
+				[133] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "132"
+				},
+				[134] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "133"
+				},
+				[135] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "134"
+				},
+				[136] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "135"
+				},
+				[137] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "136"
+				},
+				[138] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "137"
+				},
+				[139] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "138"
+				},
+				[140] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "139"
+				},
+				[141] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "140"
+				},
+				[142] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "141"
+				},
+				[143] = {
+					scaleY = "0.920",
+					scaleX = "0.920",
+					index = "142"
+				}
+			}
+		},
+		frameCount = "143",
+		name = "MDMT_Jukebox_AnimDiscPlay_d"
+	}
+}
+return xmltable;
